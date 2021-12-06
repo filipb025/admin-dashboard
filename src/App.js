2a84fcb574058788cc7723f739de5cef5cec50b2
filipb025 <filip.bogicevic025@hotmail.com>
2021-12-06 11:26:39 +0100

    react bootstrap, header & footer basic components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import { Container } from "react-bootstrap";
 function App() {
-  return <div className="">Admin</div>;
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Admin</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
