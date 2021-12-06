5b641ab433b2cdaf259e02923e119feaa0d69f2c
filipb025 <filip.bogicevic025@hotmail.com>
2021-12-06 12:00:21 +0100

    Message, Loader Components

--- src/screens/DashboardScreen.js
+++ src/screens/DashboardScreen.js
@@ -1,5 +1,6 @@
 import React from "react";
-
+import Message from "../components/Message";
+import Loader from "../components/Loader";
 const DashboardScreen = () => {
   return <div>Dashboard</div>;
 };
