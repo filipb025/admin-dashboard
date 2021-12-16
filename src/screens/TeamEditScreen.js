cf093ebed8397f8a361c34e64b5291eef56dbb4e
filipb025 <filip.bogicevic025@hotmail.com>
2021-12-16 17:43:31 +0100

    img upload setup

--- src/screens/TeamEditScreen.js
+++ src/screens/TeamEditScreen.js
@@ -6,13 +6,17 @@ import Message from "../components/Message";
 import Loader from "../components/Loader";
 import { Link } from "react-router-dom";
 import { listTeamDetails, updateTeam } from "../actions/teamActions";
-import { TEAM_UPDATE_RESET } from "../constants/teamConstants";
-
+import {
+  TEAM_UPDATE_RESET,
+  TEAM_UPDATE_REQUEST,
+} from "../constants/teamConstants";
+import axios from "axios";
 const TeamEditScreen = ({ match, history }) => {
   const teamId = match.params.id;
-
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
+  const [image, setImage] = useState("");
+  const [uploading, setUploading] = useState(false);
 
   const dispatch = useDispatch();
 
@@ -30,14 +34,37 @@ const TeamEditScreen = ({ match, history }) => {
     if (successUpdate) {
       dispatch({ type: TEAM_UPDATE_RESET });
       history.push("/teams");
-    } else {
-      if (team.id !== teamId) {
-        dispatch(listTeamDetails(teamId));
-      }
-      setName(team.name);
-      setDescription(team.description);
     }
+    dispatch(listTeamDetails(teamId));
+    setName(team.name);
+    setDescription(team.description);
   }, [dispatch, history, teamId, successUpdate]);
+
+  const uploadFileHandler = () => async (e, getState) => {
+    const file = e.target.files[0];
+    const formData = new FormData();
+    formData.append("image", file);
+    setUploading(true);
+
+    try {
+      const {
+        userLogin: { userInfo },
+      } = getState();
+
+      const config = {
+        headers: {
+          Authorization: `Bearer ${userInfo.auth.token}`,
+          "Content-Type": "multipart/form-data",
+        },
+      };
+      const { data } = await axios.post("/api/team", formData, config);
+      setImage(data);
+      setUploading(false);
+    } catch (error) {
+      console.log(error);
+      setUploading(false);
+    }
+  };
 
   const submitHandler = (e) => {
     e.preventDefault();
@@ -46,6 +73,7 @@ const TeamEditScreen = ({ match, history }) => {
         id: teamId,
         name,
         description,
+        logo: image,
       })
     );
   };
@@ -83,6 +111,19 @@ const TeamEditScreen = ({ match, history }) => {
                 value={description}
                 onChange={(e) => setDescription(e.target.value)}
               ></Form.Control>
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Image</Form.Label>
+
+              <Form.Control
+                type="file"
+                name="file"
+                custom
+                id="image"
+                label="Choose File"
+                onChange={uploadFileHandler}
+              ></Form.Control>
+              {uploading && <Loader />}
             </Form.Group>
             <Button className="my-3" type="submit" variant="primary">
               Update
